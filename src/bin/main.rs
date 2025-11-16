c324614aebd7ff2fe07f8f4f62aef240c114fe6e
Matthew Pugh <maththedude@users.noreply.github.com>
2025-11-16 14:29:22 -0600

    Added a function to flip a relay. Changed the loop to flip a relay on pin 8 each second.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,10 @@
 #![no_std]
 #![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
 use defmt::*;
 use esp_hal::{
@@ -12,6 +17,8 @@ use {esp_backtrace as _, esp_println as _};
 
 extern crate alloc;
 
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
@@ -24,16 +31,28 @@ fn main() -> ! {
 
     // Heap allocation
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 65536);
-    esp_alloc::heap_allocator!(size: 64 * 1024);
+    esp_alloc::heap_allocator!(size: 64 * 1024); //TODO: Added by esp-gen with "COEX needs more RAM - so we've added some more." Investigate.
 
     // Configure GPIO8 as output:
     let mut pin8 = Output::new(peripherals.GPIO8, Level::Low, OutputConfig::default());
 
     loop {
-        info!("Flipping");
-        pin8.toggle();
-
-        let t0 = Instant::now();
+        info!("Opening relay");
+        set_relay_state(&mut pin8, false);
+        let mut t0 = Instant::now();
         while t0.elapsed() < Duration::from_millis(1000) {}
+
+        info!("Closing relay");
+        set_relay_state(&mut pin8, true);
+        t0 = Instant::now();
+        while t0.elapsed() < Duration::from_millis(1000) {}
+    }
+}
+
+fn set_relay_state(relay_pin: &mut Output, close: bool) {
+    if close {
+        relay_pin.set_high();
+    } else {
+        relay_pin.set_low();
     }
 }
